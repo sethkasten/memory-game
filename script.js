197acc1980c5c059ddd30f273a0c4633ab6bccc9
sethkasten <kastens@umich.edu>
2021-10-15 15:09:03 -0400

    don't merge; timer code

--- script.js
+++ script.js
@@ -4,11 +4,32 @@ const startButton = document.querySelector(".start-button");
 const resetButton = document.querySelector(".reset-button");
 const main = document.querySelector(".main");
 const cardArray = [];
+const timer = document.querySelector(".timer");
+let elapsedSec = 0;
+let elapsedMin = 0;
+let elapsedHr = 0;
+let clock;
+
+const timerFunction = () => {
+  elapsedSec++;
+  timer.innerHTML = `${elapsedHr}:${elapsedMin}:${elapsedSec}`;
+  if (elapsedSec === 59) {
+    elapsedSec = 0;
+    elapsedMin++;
+  }
+  if (elapsedMin === 60) {
+    elapsedMin = 0;
+    elapsedHr++;
+  }
+};
 
 startButton.addEventListener("click", () => {
-  startButton.classList.add("hide");
+  clock = setInterval(timerFunction, 1000);
+  elapsedSec = 0;
+  elapsedMin = 0;
+  elapsedHr = 0;
 });
 
 resetButton.addEventListener("click", () => {
-  startButton.classList.remove("hide");
+  clearInterval(clock);
 });
