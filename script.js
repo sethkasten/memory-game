8b2997b49fc975f7672ab17976bd67fb4be9e5f5
sethkasten <kastens@umich.edu>
2021-10-15 15:28:02 -0400

    added timer code

--- script.js
+++ script.js
@@ -5,6 +5,24 @@ const resetButton = document.querySelector(".reset-button");
 const gameBoard = document.querySelector(".gameboard");
 const main = document.querySelector(".main");
 const cardArray = [];
+const timer = document.querySelector(".timer");
+let elapsedSec = 0;
+let elapsedMin = 0;
+let elapsedHr = 0;
+let clock;
+
+const timerFunction = () => {
+  elapsedSec++;
+  timer.innerHTML = `${elapsedHr}:${elapsedMin}:${elapsedSec}`;
+  if (elapsedSec === 59) {
+    elapsedSec = 0;
+    elapsedMin++;
+  }
+  if (elapsedMin === 60) {
+    elapsedMin = 0;
+    elapsedHr++;
+  }
+};
 
 const board = () => {
   for (let i = 0; i < 16; i++) {
@@ -26,6 +44,11 @@ startButton.addEventListener("click", () => {
     console.log(item);
     item.classList.remove("hide");
   });
+
+  clock = setInterval(timerFunction, 1000);
+  elapsedSec = 0;
+  elapsedMin = 0;
+  elapsedHr = 0;
 });
 
 resetButton.addEventListener("click", () => {
@@ -36,4 +59,6 @@ resetButton.addEventListener("click", () => {
     console.log(item);
     item.classList.add("hide");
   });
+
+  clearInterval(clock);
 });
